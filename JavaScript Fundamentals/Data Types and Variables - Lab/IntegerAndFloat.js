e19a5d711ce30fb6efe5c50bd5075dae0897b498
gkishev <georgikishev93@gmail.com>
2022-12-06 20:36:55 +0100

    exe

---
+++ JavaScript Fundamentals/Data Types and Variables - Lab/IntegerAndFloat.js
@@ -0,0 +1,16 @@
+function integerAndFloat (num1, num2, num3) {
+
+    let result = num1 + num2 + num3;
+    let resToString = String(result);
+    let isFloat = false;
+
+    for( let i = 0; i < resToString.length; i++) {
+        
+        if (resToString[i] === ".") {
+            isFloat = true;
+        }
+    }
+    console.log(`${result} - ${isFloat  ? `Float` : `Integer`}`)
+}
+
+integerAndFloat(9, 100, 1)
