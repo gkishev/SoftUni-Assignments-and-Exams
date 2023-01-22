e4492013592dca9fb6505470e2b23438e9eddb58
gkishev <georgikishev93@gmail.com>
2023-01-22 21:08:41 +0100

    exe

---
+++ JavaScript Fundamentals/Arrays - Lecture/SumFirstandLastArrayElements.js
@@ -0,0 +1,9 @@
+function SumFirstAndLastArrayElements (input) {
+
+    let first = input[0];
+    let last = input[input.length - 1];
+
+    console.log(first + last)
+}
+
+SumFirstAndLastArrayElements([20, 30, 40]);
