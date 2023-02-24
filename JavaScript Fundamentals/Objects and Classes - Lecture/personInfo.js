e865b65824b2527e50616df7d9ff2f66c0f01bc7
gkishev <georgikishev93@gmail.com>
2023-02-24 11:01:30 +0100

    exe

---
+++ JavaScript Fundamentals/Objects and Classes - Lecture/personInfo.js
@@ -0,0 +1,16 @@
+function personInfo(fName, lname, incomeAge) {
+
+    let obj = {
+        firstName: fName,
+        lastName: lname,
+        age: incomeAge,
+    }
+
+    return obj;
+}
+
+console.log(personInfo("Peter",
+
+"Pan",
+
+"20"));
