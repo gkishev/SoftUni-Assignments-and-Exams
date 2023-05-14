126e71d84b4788fec64f397fede71549f14314d6
gkishev <georgikishev93@gmail.com>
2023-05-14 20:32:29 +0200

    exe

---
+++ JavaScript Advanced/Object and Composition - Lecture/City Record.js
@@ -0,0 +1,18 @@
+function city(name, population, treasury) {
+
+    let cityInfo = {
+        name: name,
+        population: population,
+        treasury: treasury
+    }
+
+   return cityInfo;
+
+}
+
+
+console.log(city('Tortuga',
+
+7000,
+
+15000));
